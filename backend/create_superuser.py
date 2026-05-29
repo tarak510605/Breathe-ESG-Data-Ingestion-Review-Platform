9e0c9de099b1e3cb5a08577b29601b44dd8a653b
tarak510605 <tarak510605@gmail.com>
2026-05-29 20:14:25 +0530

    add vercel routing

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -1,16 +1,19 @@
 from django.contrib.auth import get_user_model
 
+print("=== CREATE SUPERUSER SCRIPT STARTED ===")
+
 User = get_user_model()
 
-print("Running create_superuser.py")
-
-if not User.objects.filter(username="admin").exists():
-    print("Creating admin user...")
-    User.objects.create_superuser(
-        username="admin",
-        email="your@email.com",
-        password="Admin123@"
-    )
-    print("Superuser created")
-else:
-    print("Superuser already exists")
+try:
+    if not User.objects.filter(username="admin").exists():
+        print("Creating admin...")
+        User.objects.create_superuser(
+            username="admin",
+            email="your@email.com",
+            password="Admin123@"
+        )
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
+except Exception as e:
+    print("ERROR:", e)
